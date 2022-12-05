8b02b8770f930192ae24d7a4e31986b2d0403e28
Rudolf Kolbe <rkolbe96@gmail.com>
2022-12-05 12:16:20 +0100

    add py 3.10 and 3.11 classifiers

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Multimedia :: Graphics",
     ],
     url="https://github.com/K0lb3/etcpak",
